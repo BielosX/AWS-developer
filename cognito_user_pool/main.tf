15da04e85896d88a504c7ecdc32b88623b1641a7
Mateusz Biel <matbiel@gmail.com>
2021-07-21 21:25:05 +0200

    Update

--- cognito_user_pool/main.tf
+++ cognito_user_pool/main.tf
@@ -80,10 +80,31 @@ resource "aws_api_gateway_deployment" "deployment" {
   }
 }
 
+resource "aws_cloudwatch_log_group" "api-gateway-access-log-group" {
+  name = "api-gw-access-logs"
+}
+
+resource "aws_iam_role" "api-gateway-logs-role" {
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_api_gw_logs" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+  role = aws_iam_role.api-gateway-logs-role.id
+}
+
+resource "aws_api_gateway_account" "acc" {
+  cloudwatch_role_arn = aws_iam_role.api-gateway-logs-role.arn
+}
+
 resource "aws_api_gateway_stage" "prod" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name = "prod"
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api-gateway-access-log-group.arn
+    format = "$context.identity.sourceIp,$context.identity.caller,$context.identity.user,$context.requestTime,$context.httpMethod,$context.resourcePath,$context.protocol,$context.status,$context.responseLength,$context.requestId,$context.integrationErrorMessage,$context.error.message"
+  }
 }
 
 resource "aws_cognito_user_pool_client" "web-client" {
