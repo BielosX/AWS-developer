6e01d5c0a9ac6100dbadb002e3a47ee3725bf427
Mateusz Biel <matbiel@gmail.com>
2021-07-21 16:23:03 +0200

    Setup api gateway proxy to S3 bucket

---
+++ cognito_user_pool/main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_iam_policy_document" "api_gateway_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["apigateway.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "s3_access_role" {
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  role = aws_iam_role.s3_access_role.id
+}
+
+resource "aws_s3_bucket" "web_bucket" {
+  acl = "public-read"
+  force_destroy = true
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "web_page_html" {
+  for_each = fileset("${path.module}/src", "*.html")
+  content = file("${path.module}/src/${each.value}")
+  bucket = aws_s3_bucket.web_bucket.id
+  content_type = "text/html"
+  key = each.value
+  acl = "public-read"
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "demo-api"
+  body = templatefile("${path.module}/openapi.json.tmpl", {
+    s3_access_role = aws_iam_role.s3_access_role.arn
+    bucket_name = aws_s3_bucket.web_bucket.id
+    region = "eu-west-1"
+  })
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    redeployment = sha1(aws_api_gateway_rest_api.api.body)
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name = "prod"
+}
