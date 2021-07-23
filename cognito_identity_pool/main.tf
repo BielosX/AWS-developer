6bf4a67a429fbd3cdef85f18043668696e7250a4
Mateusz Biel <matbiel@gmail.com>
2021-07-23 18:15:19 +0200

    Update

--- cognito_identity_pool/main.tf
+++ cognito_identity_pool/main.tf
@@ -58,3 +58,63 @@ resource "aws_cognito_identity_pool" "demo-identity-pool" {
     server_side_token_check = false
   }
 }
+
+resource "aws_sqs_queue" "demo-queue" {
+  visibility_timeout_seconds = 60
+}
+
+data "aws_iam_policy_document" "assume-role" {
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["cognito-identity.amazonaws.com"]
+      type = "Federated"
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test = "StringEquals"
+      values = [aws_cognito_identity_pool.demo-identity-pool.id]
+      variable = "cognito-identity.amazonaws.com:aud"
+    }
+    condition {
+      test = "ForAnyValue:StringLike"
+      values = ["authenticated"]
+      variable = "cognito-identity.amazonaws.com:amr"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "demo-queue-full-access" {
+  statement {
+    effect = "Allow"
+    actions = ["sqs:*"]
+    resources = [aws_sqs_queue.demo-queue.arn]
+  }
+}
+
+resource "aws_iam_role" "sqs-access-role" {
+  assume_role_policy = data.aws_iam_policy_document.assume-role.json
+}
+
+resource "aws_iam_role_policy" "demo-queue-full-access" {
+  policy = data.aws_iam_policy_document.demo-queue-full-access.json
+  role = aws_iam_role.sqs-access-role.id
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "role-attachment" {
+  identity_pool_id = aws_cognito_identity_pool.demo-identity-pool.id
+  role_mapping {
+    identity_provider = "${aws_cognito_user_pool.demo-user-pool.endpoint}:${aws_cognito_user_pool_client.demo-client.id}"
+    ambiguous_role_resolution = "AuthenticatedRole"
+    type = "Rules"
+    mapping_rule {
+      claim = "isAdmin"
+      match_type = "Equals"
+      role_arn = aws_iam_role.sqs-access-role.arn
+      value = "paid"
+    }
+  }
+  roles = {
+    "authenticated" = aws_iam_role.sqs-access-role.arn
+  }
+}
--- cognito_identity_pool/sqs_publish.py
+++ cognito_identity_pool/sqs_publish.py
@@ -3,6 +3,28 @@ import argparse
 import base64
 import json
 
+def get_identity_id(account_id, identity_pool_id, user_pool, token):
+    body = {
+        'AccountId': account_id,
+        'IdentityPoolId': identity_pool_id,
+        'Logins': {
+            user_pool: token
+        }
+    }
+    conn = http.client.HTTPSConnection("cognito-identity.eu-west-1.amazonaws.com", 443)
+    conn.request('POST', "",
+        headers={
+            'Content-Type': 'application/x-amz-json-1.1',
+            'x-amz-target': 'com.amazonaws.cognito.identity.model.AWSCognitoIdentityService.GetId'
+        },
+        body=json.dumps(body)
+    )
+    resp = conn.getresponse()
+    data = resp.read()
+    conn.close()
+    print(data)
+    return json.loads(data)['IdentityId']
+
 def get_token(url, client_id, client_secret):
     conn = http.client.HTTPSConnection(url, 443)
     basic = base64.b64encode('{}:{}'.format(client_id, client_secret).encode('ascii')).decode('ascii')
@@ -22,9 +44,13 @@ def main():
     parser.add_argument('--url')
     parser.add_argument('--id')
     parser.add_argument('--secret')
+    parser.add_argument('--user-pool')
+    parser.add_argument('--account-id')
+    parser.add_argument('--identity-pool-id')
     args = vars(parser.parse_args())
     token = get_token(args['url'], args['id'], args['secret'])
-    print(token)
+    identity_id = get_identity_id(args['account_id'], args['identity_pool_id'], args['user_pool'], token)
+    print(identity_id)
 
 if __name__ == "__main__":
     main()
