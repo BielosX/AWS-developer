c81a7467d7351ad0daade59bf2f4914eb5cde24f
Mateusz Biel <matbiel@gmail.com>
2021-09-23 16:16:26 +0200

    Allow ec2 instance to use S3 bucket with instances ip address prefix

---
+++ iam/main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "vpc" {
+  source = "./vpc"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+resource "aws_s3_bucket" "demo-bucket" {
+  bucket = "iam-demo-bucket-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
+  force_destroy = true
+  acl = "private"
+}
+
+resource "aws_security_group" "demo-security-group" {
+  vpc_id = module.vpc.vpc_id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+  }
+}
+
+resource "aws_eip" "demo-eip" {
+  vpc = true
+}
+
+data "aws_iam_policy_document" "ec2-assume-role" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type        = "Service"
+    }
+    condition {
+      test     = "IpAddress"
+      values   = [aws_eip.demo-eip.public_ip]
+      variable = "aws:SourceIp"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ec2-s3-bucket-access" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.demo-bucket.arn]
+    condition {
+      test     = "StringLike"
+      values   = ["$${aws:SourceIp}/*"]
+      variable = "s3:prefix"
+    }
+    condition {
+      test     = "IpAddress"
+      values   = [aws_eip.demo-eip.public_ip]
+      variable = "aws:SourceIp"
+    }
+  }
+  statement {
+    effect = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject"]
+    resources = ["${aws_s3_bucket.demo-bucket.arn}/$${aws:SourceIp}/*"]
+    condition {
+      test     = "IpAddress"
+      values   = [aws_eip.demo-eip.public_ip]
+      variable = "aws:SourceIp"
+    }
+  }
+}
+
+resource "aws_iam_role" "demo-role" {
+  assume_role_policy = data.aws_iam_policy_document.ec2-assume-role.json
+  inline_policy {
+    name = "DemoPolicy"
+    policy = data.aws_iam_policy_document.ec2-s3-bucket-access.json
+  }
+}
+
+data "aws_ami" "amazon-linux-2" {
+  owners = ["amazon"]
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+resource "aws_iam_instance_profile" "instance-profile" {
+  role = aws_iam_role.demo-role.name
+}
+
+resource "aws_instance" "demo-instance" {
+  ami = data.aws_ami.amazon-linux-2.id
+  subnet_id = module.vpc.public_subnet_id
+  associate_public_ip_address = true
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.demo-security-group.id]
+  iam_instance_profile = aws_iam_instance_profile.instance-profile.name
+}
+
+resource "aws_eip_association" "ec2-eip-assoc" {
+  allocation_id = aws_eip.demo-eip.id
+  instance_id = aws_instance.demo-instance.id
+}
---
+++ iam/vpc/main.tf
@@ -0,0 +1,39 @@
+resource "aws_vpc" "simple_vpc" {
+  cidr_block = "10.0.0.0/22"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "SimpleVPC"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public_subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.simple_vpc.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.simple_vpc.id
+}
+
+resource "aws_route_table" "vpc_public_route_table" {
+  vpc_id = aws_vpc.simple_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "public_table_assoc" {
+  route_table_id = aws_route_table.vpc_public_route_table.id
+  subnet_id = aws_subnet.public_subnet.id
+}
